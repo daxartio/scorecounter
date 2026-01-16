c8a0318ef9845143ae26081fc7b0a4454b93d586
daxartio <daxartio@users.noreply.github.com>
2026-01-16 11:52:25 +0000

    deploy: 8035af85d70d46a887aebdf5f16802712102725d

--- assets/sw-register-dxh6618952c3dda75b6.js
+++
@@ -1,1 +0,0 @@
-if("serviceWorker"in navigator){window.addEventListener("load",()=>{navigator.serviceWorker.register("/assets/service-worker.js",{scope:"/"}).catch(err=>console.error("SW registration failed",err));});}
